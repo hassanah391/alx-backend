cc6060d1e5eea564f64c126b4a9016c71591d931
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-16 09:59:07 +0300

    Removes indentations for get()

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -28,14 +28,14 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
         self.cache_data[key] = item
 
-        def get(self, key):
-            """Retrieve an item by key from the cache.
+    def get(self, key):
+        """Retrieve an item by key from the cache.
 
-            Args:
-                key: The key of the item to retrieve.
-            Returns:
-                The item if found, else None.
+        Args:
+            key: The key of the item to retrieve.
+        Returns:
+            The item if found, else None.
             """
-            if key is None or key not in self.cache_data.keys():
-                return None
-            return self.cache_data[key]
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
